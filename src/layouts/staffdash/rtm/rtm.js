03a0d47a0e2b6949992f39ccdc19613e14645039
shivaraj shankar <51598598+shivaraj65@users.noreply.github.com>
2021-06-06 19:44:30 +0530

    final patch of the project

--- src/layouts/staffdash/rtm/rtm.js
+++ src/layouts/staffdash/rtm/rtm.js
@@ -5,11 +5,22 @@ import Button from 'react-bootstrap/Button'
 
 const Rtm=()=>{
     const [data,setData]=useState(null)
+    //holds the candidates exam data of selected examID
+    const [eCandiData,setECandiData]=useState(null)
 
     // states and function for the modal
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false); 
     const handleShow = () => setShow(true);
+
+    //holds the popup page configuration
+    const [pageno,setPageno]=useState(1)
+
+    //set image links for the selected candidates
+    const [imageData,setImageData]=useState(null)
+
+    //state to hold the actual user data informations
+    const [userProfile,setUserProfile]=useState(null)
 
     useEffect(()=>{
         // axios 
@@ -21,7 +32,7 @@ const Rtm=()=>{
             axios.post('https://7vqla7ygik.execute-api.us-east-1.amazonaws.com/production', 
             JSON.stringify(json),config)
             .then(function (response) {
-              console.log(response.data)
+            //   console.log(response.data)
               if(response.data.status==="success"){
                 setData(response.data.message.Items)
               }else{
@@ -33,6 +44,55 @@ const Rtm=()=>{
                 console.log("error")
             });   
     },[])
+
+    const fetchExamAnsData=(id)=>{
+        // axios 
+        const json = {eid:id};  
+        const config  = {
+            headers: {
+               'Content-Type': 'application/json',
+            }}
+            axios.post('https://67xwscju4a.execute-api.us-east-1.amazonaws.com/production', 
+            JSON.stringify(json),config)
+            .then(function (response) {
+            //   console.log(response.data)
+              if(response.data.status==="success"){
+                setECandiData(response.data.message.Items)
+              }else{
+                  alert("something went wrong refresh or try later!")
+              }              
+            })
+            .catch(function (error) {
+                alert("something went wrong. Please try again.")
+                console.log("error")
+            });
+    }
+
+    const fetchUserData=(idd)=>{
+        //fetch the user data
+        // https://zj28gizx9b.execute-api.us-east-1.amazonaws.com/production
+        // axios 
+        const json = {id:idd};  
+        const config  = {
+            headers: {
+               'Content-Type': 'application/json',
+            }}
+            axios.post('https://zj28gizx9b.execute-api.us-east-1.amazonaws.com/production', 
+            JSON.stringify(json),config)
+            .then(function (response) {
+            //   console.log(response.data)
+              if(response.data.status==="success"){
+                setUserProfile(response.data.payload.Item)
+                console.log(response.data.payload.Item)
+              }else{
+                  alert("something went wrong refresh or try later!")
+              }              
+            })
+            .catch(function (error) {
+                alert("something went wrong. Please try again.")
+                console.log("error")
+            });
+    }
 
     return(
         <div>
@@ -52,6 +112,7 @@ const Rtm=()=>{
                                 className="btn btn-sm"
                                 onClick={()=>{
                                     handleShow()
+                                    fetchExamAnsData(entry.examID)
                                 }}
                                 ><img src="https://img.icons8.com/nolan/64/info-popup.png" alt="" style={{maxHeight:"32px"}}/></button>
                         </div>
@@ -73,16 +134,127 @@ const Rtm=()=>{
                     keyboard={false}
                     centered
                     size="lg"
+                    scrollable={true}
                 >
                 <Modal.Header closeButton>
                     <Modal.Title>&lt;meriTTrack&gt;</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <h5 className="text-primary">it0001</h5>
-                    <p>check for back end</p>
-                    <p>route the backend</p>
-                    <p>fetch list of students attended the particular exam</p>
-                    <p>fetch the particular candidates exam data -exam images +personal info from the bio</p>
+                    {pageno===1?
+                        <div>
+                            <div className="px-1">
+                            <div className="row mt-4 fonr-weight-bold">
+                                <div className="col-sm-2 pt-3 pb-1">
+                                    <p className="text-dark">SID</p>
+                                </div>
+                                <div className="col-sm-2 pt-3 pb-1">
+                                    <p className="text-dark">Credibility</p>
+                                </div>
+                                <div className="col-sm-2 pt-3 pb-1">
+                                    <p className="text-dark">Malpractice</p>
+                                </div>
+                                <div className="col-sm-2 pt-3 pb-1">
+                                    <p className="text-dark">score</p>
+                                </div>
+                                <div className="col-sm-2 pt-3 pb-1">
+                                    <p className="text-dark">submission type</p>
+                                </div>
+                                <div className="col-sm-2 pt-3 pb-1">
+                                    <p className="text-dark">more data</p>
+                                </div>
+                            </div>
+                    {eCandiData && eCandiData.map((entry,index)=>{
+                        return(
+                            <div className="row mt-4 hovered" key={index}>
+                                <div className="col-sm-2 pt-1 pb-2">
+                                    <p className="text-secondary">{entry.sid}</p>
+                                </div>
+                                <div className="col-sm-2 pt-1 pb-2">
+                                    <p className="text-secondary">{entry.credibility}</p>
+                                </div>
+                                <div className="col-sm-2 pt-1 pb-2">
+                                    <p className="text-secondary">
+                                        {entry.malprac===true?"true":"false"}
+                                    </p>
+                                </div>
+                                <div className="col-sm-2 pt-1 pb-2">
+                                    <p className="text-secondary">{entry.score}</p>
+                                </div>
+                                <div className="col-sm-2 pt-1 pb-2">
+                                    <p className="text-secondary">{entry.submitType}</p>
+                                </div>
+                                <div className="col-sm-2 text-center">
+                                    <button 
+                                        className="btn btn-sm"
+                                        onClick={()=>{
+                                            setPageno(2)
+                                            setImageData(entry.facereck)
+                                            console.log(entry.facereck)
+                                            fetchUserData(entry.sid)
+                                        }}
+                                        ><img src="https://img.icons8.com/nolan/64/info-popup.png" alt="" style={{maxHeight:"32px"}}/></button>
+                                </div>
+                            </div>
+                        )})
+                    }
+                    </div>
+                    {eCandiData===null?
+                    <div className="m-4">
+                        <div className="spinner-border text-warning" role="status">
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    </div>:null}
+                    </div>:null}
+                    {/* //----end of page1----// */}
+                    {pageno===2?<div>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                <h6 className="text-center text-info">Candidate Details</h6>
+                            </div>
+                            <div className="col-sm-6">
+                                <h6 className="text-danger text-center">Examination Data</h6> 
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-sm-6">
+                                {userProfile?<div className="">
+                                    <div className="text-center">
+                                        <img src={userProfile.imageurl} alt="" className="custom-image-1"/>
+                                    </div>                                    
+                                    <div className="row ml-1">
+                                        <div className="col-sm-5">
+                                            <h6 className="text-dark">Register no</h6>
+                                            <h6 className="text-dark">Name</h6>
+                                            <h6 className="text-dark">Dept</h6>
+                                            <h6 className="text-dark">Email</h6>
+                                        </div>
+                                        <div className="com-md-7">
+                                            <h6 className="text-success">{": "+userProfile.registernumber}</h6>
+                                            <h6 className="text-success">{": "+userProfile.name}</h6>
+                                            <h6 className="text-success">{": "+userProfile.department}</h6>
+                                            <h6 className="text-success">{": "+userProfile.email}</h6>
+                                        </div>
+                                    </div>
+                                </div>:null}
+                                {/* show the profile data here */}
+                            </div>
+                            <div className="col-sm-6 text-center overflow-control border border-primary rounded">                            
+                                {imageData && imageData.map((entry,index)=>{
+                                    return(
+                                        <div>
+                                            <img src={entry.url} alt="" className="custom-image" key={index}/>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        <button 
+                            className="btn btn-sm btn-success ml-3 px-4"
+                            onClick={()=>{
+                                setPageno(1)
+                            }}
+                            >back</button>
+                    </div>:null}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="danger" onClick={handleClose}>
